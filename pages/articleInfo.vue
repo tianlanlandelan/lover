d0b62ad637c0de04ddb540e77d5e082e4f2c612b
yangkaile <guyexing@foxmail.com>
2019-08-10 08:35:22 +0800

    添加文章详情

--- pages/articleInfo.vue
+++ pages/articleInfo.vue
@@ -1,7 +1,30 @@
 <template>
+	<view>
+		 
+		 <view class="uni-common-mt" style="background:#FFF; padding:20rpx;">
+                <rich-text :nodes="strings"></rich-text>
+         </view>
+	</view>
 </template>
 
 <script>
+	export default {
+		onLoad: function (option) { 
+			//打印出上个页面传递的参数
+			console.log(option.id,option.title); 
+			//设置导航栏标题
+			uni.setNavigationBarTitle({
+				title: option.title
+			});
+			//TODO 发送请求，获取页面显示数据
+			
+		},
+		data(){
+			return{
+				strings:'<div style="text-align:center;"><img src="https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/uni@2x.png"/><p>这是文章内容</p></div>'
+			}
+		}
+	}
 </script>
 
 <style>
--- pages/index/find.vue
+++ pages/index/find.vue
@@ -2,15 +2,15 @@
 	<view>
 		<view class="uni-flex uni-row">
 			<view class="kyle-2-1">
-				<view class="kyle-padding-sm" v-for="story in stories" v-if="story.id % 2 === 0" :key="story.id" @click="clickCard(story.id)">
+				<view class="kyle-padding-sm" v-for="story in stories" v-if="story.id % 2 === 0" :key="story.id" @click="goDetailPage(story)">
 					<image class="kyle-1-1 kyle-radius" :src="story.img"></image>
-					<text>{{story.text}}</text>
+					<text>{{story.title}}</text>
 				</view>
 			</view>
 			<view class="kyle-2-1">
-				<view class="kyle-padding-sm" v-for="story in stories" v-if="story.id % 2 === 1" :key="story.id" @click="clickCard(story.id)">
+				<view class="kyle-padding-sm" v-for="story in stories" v-if="story.id % 2 === 1" :key="story.id" @click="goDetailPage(story)">
 					<image class="kyle-1-1 kyle-radius" :src="story.img"></image>
-					<text>{{story.text}}</text>
+					<text>{{story.title}}</text>
 				</view>
 			</view>		
 		</view>
@@ -25,22 +25,25 @@
 		data() {
 			return {
 				stories:[
-					{id:1,img:"../../static/img/img1.jpg",text:"520表白神器"},
-					{id:2,img:"../../static/img/img2.jpg",text:"想要挽回爱人，我们必须注重的一些挽回技巧"},
-					{id:3,img:"../../static/img/img3.jpg",text:"教你看出别人是不是暗恋你"},
-					{id:4,img:"../../static/img/img4.jpg",text:"恋爱专属表情包"},
-					{id:5,img:"../../static/img/img1.jpg",text:"恋爱专属表情包"},
-					{id:6,img:"../../static/img/img2.jpg",text:"恋爱专属表情包"},
-					{id:7,img:"../../static/img/img3.jpg",text:"恋爱专属表情包"},
-					{id:8,img:"../../static/img/img4.jpg",text:"恋爱专属表情包"},
-					{id:9,img:"../../static/img/img1.jpg",text:"恋爱专属表情包"},
-					{id:10,img:"../../static/img/img2.jpg",text:"恋爱专属表情包"}
+					{id:1,img:"../../static/img/img1.jpg",title:"520表白神器"},
+					{id:2,img:"../../static/img/img2.jpg",title:"想要挽回爱人，我们必须注重的一些挽回技巧"},
+					{id:3,img:"../../static/img/img3.jpg",title:"教你看出别人是不是暗恋你"},
+					{id:4,img:"../../static/img/img4.jpg",title:"恋爱专属表情包"},
+					{id:5,img:"../../static/img/img1.jpg",title:"恋爱专属表情包"},
+					{id:6,img:"../../static/img/img2.jpg",title:"恋爱专属表情包"},
+					{id:7,img:"../../static/img/img3.jpg",title:"恋爱专属表情包"},
+					{id:8,img:"../../static/img/img4.jpg",title:"恋爱专属表情包"},
+					{id:9,img:"../../static/img/img1.jpg",title:"恋爱专属表情包"},
+					{id:10,img:"../../static/img/img2.jpg",title:"恋爱专属表情包"}
 				]
 			}
 		},
 		methods:{
-			clickCard(id){
-				console.info(id);
+			goDetailPage(story) {
+				const pageUrl = "/pages/articleInfo?id="+story.id+"&title="+story.title;
+				uni.navigateTo({
+					url: pageUrl
+				})
 			}
 		}
 	}
