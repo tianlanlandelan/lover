ad8d0c37232a76f5925383e778cb7a1caf75cdd6
yangkaile <guyexing@foxmail.com>
2019-08-11 11:34:10 +0800

    ty

--- pages/index/me.vue
+++ pages/index/me.vue
@@ -2,11 +2,11 @@
 	<view>
 		<view class="kyle-padding kyle-radius">		
 			<uni-list>
-				<uni-list-item title="我的收藏" thumb="../../static/bar/school1.png" />
-				<uni-list-item title="客服反馈" thumb="../../static/bar/school1.png" />
-				<uni-list-item title="账号安全" thumb="../../static/bar/school1.png" />
-				<uni-list-item title="使用帮助" thumb="../../static/bar/school1.png" />
-				<uni-list-item title="设置" thumb="../../static/bar/school1.png" />
+				<uni-list-item title="我的收藏" thumb="../../static/icon/guanzhu.png" />
+				<uni-list-item title="客服反馈" thumb="../../static/icon/kefu.png" />
+				<uni-list-item title="账号安全" thumb="../../static/icon/safe.png" />
+				<uni-list-item title="使用帮助" thumb="../../static/icon/ziliao.png" />
+				<uni-list-item title="设置" thumb="../../static/icon/set.png" />
 			</uni-list>
 		</view>
 	</view>
